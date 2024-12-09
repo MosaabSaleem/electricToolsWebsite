ed6429e98744144f8071f6dcdd9ec65b8f9ac0d3
MosaabSaleem <mmosaab2002@hotmail.com>
2024-12-09 22:15:24 +1100

    Made login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import ProductPage from './Pages/ProductPage';
 import Login from './Pages/LoginPage';
 import Admin from './Pages/AdminPage';
 import Checkout from './Pages/CheckoutPage';
+import Signup from './Pages/SignupPage';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<Admin />} />
         <Route path='/checkout' element={<Checkout/>} />
       </Routes>
--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Input } from "@mui/material";
+import "../Styles/Login.css";
 
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  const navigate = useNavigate();
+  const handleSignUpClick = () => {
+    navigate("/signup");
+  };
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginClick = () => {
+    alert("login");
+  };
+
+  return (
+    <div className="loginBody">
+      <div className="middleContainer">
+        <div className="titleDiv">
+          <h1>Please Login or Sign up</h1>
+        </div>
+        
+        <div className="inputDiv">
+          <Input className="loginInput" placeholder="Email"></Input>
+          <Input
+            className="loginInput"
+            placeholder="Password"
+            type="password"
+          ></Input>
+        </div>
+        
+        <Button variant="contained" className='loginButton' sx={{paddingTop: "10px"}} onClick={handleLoginClick}>Login</Button>
+        <Button variant="contained" className='loginButton' sx={{marginBottom: "10px"}} onClick={handleSignUpClick}>Sign up</Button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
