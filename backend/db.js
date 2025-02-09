f22813882dcb56196b068f41d6b618e68a9cced7
MosaabSaleem <mmosaab2002@hotmail.com>
2025-02-09 18:21:58 +1100

    changed some database config settings

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(/*process.env.MONGO_URI*/process.env.AZURE_COSMOS_DATABASE, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(/*process.env.MONGO_URI*/process.env.AZURE_COSMOS_DATABASE//, {
+        //useNewUrlParser: true,
+        //useUnifiedTopology: true,
+    //}
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
