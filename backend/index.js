634bfc4ee0a76954e83dce8a40dc75a1e6d6361e
MosaabSaleem <mmosaab2002@hotmail.com>
2025-02-10 21:40:58 +1100

    changed domain

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connectDB = require("./db");
 const Product = require("./models/Product");
 const User = require("./models/User");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const router = express.Router();
 const jwt = require("jsonwebtoken");
@@ -23,7 +23,7 @@ app.use(cors({
 
 app.use(express.static("public"));
 
-const Domain = process.env.My_Domain;
+const Domain = process.env.Hosted_Domain;
 
 app.post('/create-checkout-session', async (req, res) => {
   const {items} = req.body;
